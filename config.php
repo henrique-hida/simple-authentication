6ad287132de5727b0fc94e99d028d4a19521271a
Henrique Hida <henriquehida@iMac-de-Henrique.local>
2024-11-01 23:33:14 -0300

    Atualizando configurações para conexão com o banco de dados no Render

--- config.php
+++ config.php
@@ -1,10 +1,21 @@
 <?php
 
-    $dbHost = 'localhost';
-    $dbUsername = 'root';
-    $dbPassword = '';
-    $dbName = 'formulario-henrique';
+    $dbHost = 'dpg-csiojmlsvqrc73ehbrbg-a'; // Hostname do Render
+    $dbPort = '5432'; // Porta padrão do PostgreSQL
+    $dbUsername = 'postgre_database_yo4j_user'; // Nome do usuário
+    $dbPassword = 'sarBM9vOD5xMxt3Pa6HdkLZwNcXckL8m'; // Senha
+    $dbName = 'postgre-database'; // Nome do banco de dados
 
-    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+    // Montando a string de conexão
+    $connectionString = "host=$dbHost port=$dbPort dbname=$dbName user=$dbUsername password=$dbPassword";
 
+    // Conectando ao banco de dados
+    $conexao = pg_connect($connectionString);
+
+    // Verificando a conexão
+    if (!$conexao) {
+        die("Falha na conexão: " . pg_last_error());
+    } else {
+        echo "Conexão bem-sucedida!";
+    }
 ?>
